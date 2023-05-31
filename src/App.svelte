51b1737a88bde4d600814ec6590f429d51832173
CODIFIEST <surfdean808@gmail.com>
2023-05-31 14:57:26 -0700

    first love

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  async function connectMetaMask() {
+    console.log('wallet button clicked')
+
+  }
+ </script>
+
+<main>
+
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">I LOVE</h1>
+      <p class="py-6">Claim your personal love ens here.
+        <br> Buy with LOVE.
+        <br> Share your LOVE ens.</p>
+        <label for="my-modal" class="btn">Connect Wallet</label>
+      <!-- <button on:click={connectMetaMask} class="btn btn-primary">Connect Wallet</button> -->
+    </div>
+  </div>
+</div>
+
+</main>
+<input type="checkbox" id="my-modal" class="modal-toggle" />
+<div class="modal">
+  <div class="modal-box">
+    <div style="width:100%;height:0;padding-bottom:85%;position:relative;"><iframe src="https://giphy.com/embed/g7GKcSzwQfugw" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+    <!-- <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  -->
+  <div class="modal-action">
+    <label for="my-modal" class="btn">Yay!</label>
+  </div> 
+  </div>
+</div>
+
+<style>
+</style>
---
+++ src/pages/LoginPage.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import { push } from "svelte-spa-router";
+    // import Hero from "../../components/Hero.svelte";
+    // import HighScoreList from "../../components/HighScoreList.svelte";
+    // import connectPhantom from "../../utils/connectPhantom";
+import connectMetamask from "../../utils/connectMetamask";
+    // import nfts from "../stores/nfts";
+
+    // import connectPhantom from "../utils/connectPhantom"
+    </script>
+    
+    <div style="display: block;">
+        <h1 class ="card-title text-secondary text-2xl">Connect your wallet to choose your NGMI Panda</h1><br>
+    
+    
+    <div class="nft-wallets">
+    
+         <!-- <img 
+            class = "wallet-image"
+            alt = "phantom wallet"
+            src = 'phantom-icon-purple.png'
+            on:click={
+                async ()=>{
+                    await connectPhantom();
+                    await push ('/displaychars');
+                }
+            }
+            on:keypress={
+                async ()=>{
+                    await connectPhantom();
+                }
+            }
+        > -->
+        <img
+        class="wallet-image"
+        src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
+        alt="metamask Fox"
+        on:keypress={async () => {
+            await connectMetamask()
+            // .then(async ()=>{
+
+            // });
+         
+        }}
+        on:click={async () => {
+            await connectMetamask();
+            console.log('nfts from login page',$nfts);
+                await push ('/displaychars');
+        }}
+    />
+    
+    </div>
+
+    </div>
+   
+    <style>
+    </style>
+    
